eff940003ff630bfacded1dd486bbe32a9e58328
Houssem Nasri <housi.housi2015@gmail.com>
2022-08-12 15:43:08 +0100

    Greet users with their name

--- src/main/java/net/askvio/AppController.java
+++ src/main/java/net/askvio/AppController.java
@@ -1,13 +1,14 @@
 package net.askvio;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 
     @GetMapping
-    public String homePage() {
-        return "<h1> Hello World </h1>";
+    public String homePage(@RequestParam(defaultValue = "World") String name) {
+        return "<h1> Hello %s </h1>".formatted(name);
     }
 }
