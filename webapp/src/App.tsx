5bfda0d21479518fdff02dc1acea4702e818e25e
Houssem Nasri <housi.housi2015@gmail.com>
2022-08-29 02:42:16 +0100

    Design the login page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Box, Button, Container} from "@mui/material";
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+
+const theme = createTheme()
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                    <Nav/>
+                    <main>
+                        <Login/>
+                    </main>
+                </Box>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
