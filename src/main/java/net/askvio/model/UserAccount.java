fe2941222c0797e11b37b40f94ca1038335ccda0
Houssem Nasri <housi.housi2015@gmail.com>
2022-08-30 02:49:24 +0100

    Define UserAccount properties

---
+++ src/main/java/net/askvio/model/UserAccount.java
@@ -0,0 +1,35 @@
+package net.askvio.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @NotBlank(message = "Firstname is required")
+    private String firstname;
+    @NotBlank(message = "Lastname is required")
+    private String lastname;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String hashedPassword;
+    private Instant creationDate;
+    private Boolean activated;
+}
