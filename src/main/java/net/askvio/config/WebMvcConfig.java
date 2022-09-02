04b4a742cc13e92bff8a996fa738890d13bdb141
Houssem Nasri <housi.housi2015@gmail.com>
2022-09-02 04:17:09 +0100

    Disable Spring security default CORS policy

---
+++ src/main/java/net/askvio/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package net.askvio.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
+
