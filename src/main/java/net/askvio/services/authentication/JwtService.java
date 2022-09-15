27a7fce95479aa210b04e80f7c05cc59691be560
Houssem Nasri <housi.housi2015@gmail.com>
2022-09-15 14:20:27 +0100

    Add methods to validate a JWT and to extract email from a JWT

--- src/main/java/net/askvio/services/authentication/JwtService.java
+++ src/main/java/net/askvio/services/authentication/JwtService.java
@@ -4,13 +4,19 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class JwtService {
     private static final Duration TOKEN_DURATION = Duration.ofHours(24);
     @Value("${app.jwt.secret}")
@@ -21,5 +27,28 @@ public class JwtService {
                    .setIssuedAt(Date.from(Instant.now()))
                    .setExpiration(Date.from(Instant.now().plus(TOKEN_DURATION)))
                    .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public boolean validateJwtToken(String jwt) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(jwt);
+            return true;
+        } catch (ExpiredJwtException ex) {
+            log.error("JWT expired", ex);
+        } catch (IllegalArgumentException ex) {
+            log.error("Token is null, empty or only whitespace", ex);
+        } catch (MalformedJwtException ex) {
+            log.error("JWT is invalid", ex);
+        } catch (UnsupportedJwtException ex) {
+            log.error("JWT is not supported", ex);
+        } catch (SignatureException ex) {
+            log.error("Signature validation failed");
+        }
+
+        return false;
+    }
+
+    public String extractEmailFromJWT(String jwt) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJwt(jwt).getBody().getSubject();
     }
 }
