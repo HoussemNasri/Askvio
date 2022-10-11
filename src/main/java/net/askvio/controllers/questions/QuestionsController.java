6be3d088e159cec940c4b9ca39ad96b044df40ad
Houssem Nasri <housi.housi2015@gmail.com>
2022-10-11 19:56:21 +0100

    Add an API for submitting questions

--- src/main/java/net/askvio/controllers/questions/QuestionsController.java
+++ src/main/java/net/askvio/controllers/questions/QuestionsController.java
@@ -1,9 +1,27 @@
 package net.askvio.controllers.questions;
 
+import lombok.AllArgsConstructor;
+import net.askvio.controllers.questions.dto.QuestionResponse;
+import net.askvio.controllers.questions.dto.SubmitQuestionRequest;
+import net.askvio.services.questions.QuestionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/questions")
 public class QuestionsController {
+
+    private final QuestionService questionService;
+
+    @PostMapping
+    public ResponseEntity<QuestionResponse> submitQuestion(@RequestBody SubmitQuestionRequest submitQuestionRequest, Authentication authentication) {
+        return questionService.submitQuestion(submitQuestionRequest, authentication)
+                              .map(ResponseEntity::ok)
+                              .orElse(ResponseEntity.badRequest().build());
+    }
 }
---
+++ src/main/java/net/askvio/services/questions/QuestionService.java
@@ -0,0 +1,67 @@
+package net.askvio.services.questions;
+
+import java.time.Instant;
+import java.util.Objects;
+import java.util.Optional;
+
+import lombok.AllArgsConstructor;
+import net.askvio.controllers.communities.CommunityResponse;
+import net.askvio.controllers.questions.dto.QuestionResponse;
+import net.askvio.controllers.questions.dto.SubmitQuestionRequest;
+import net.askvio.database.CommunityRepository;
+import net.askvio.database.QuestionRepository;
+import net.askvio.database.UserAccountRepository;
+import net.askvio.exceptions.NotImplementedException;
+import net.askvio.model.Community;
+import net.askvio.model.Question;
+import net.askvio.model.UserAccount;
+import net.askvio.services.account.UserAccountService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+
+@AllArgsConstructor
+public class QuestionService {
+
+    private final QuestionTitleNormalizer questionTitleNormalizer;
+    private final UserAccountService userAccountService;
+
+    private final UserAccountRepository userAccountRepository;
+    private final QuestionRepository questionRepository;
+    private final CommunityRepository communityRepository;
+
+    @Value("${react-app.url}")
+    private final String reactAppUrl;
+
+    public Optional<QuestionResponse> submitQuestion(SubmitQuestionRequest request, Authentication authentication) {
+        Optional<UserAccount> principalAccount = userAccountService.getPrincipalUserAccount(authentication);
+        if (principalAccount.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Optional<Community> community = communityRepository.findById(request.communityId());
+        if (community.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Question submittedQuestion = new Question(null, request.title(),
+                request.content(), Instant.now(), 0, principalAccount.get(), community.get());
+        submittedQuestion = questionRepository.save(submittedQuestion);
+
+        return Optional.of(new QuestionResponse(
+                submittedQuestion.getId(),
+                submittedQuestion.getTitle(),
+                submittedQuestion.getContent(),
+                submittedQuestion.getCreationDate(),
+                userAccountRepository.findUserResponseDTOByEmail(principalAccount.get().getEmail()).orElseThrow(),
+                communityRepository.findCommunityById(request.communityId()).orElseThrow(),
+                generateQuestionLink(submittedQuestion.getId(), submittedQuestion.getTitle())
+        ));
+    }
+
+    public String generateQuestionLink(Long questionId, String questionTitle) {
+        Objects.requireNonNull(questionId);
+        Objects.requireNonNull(questionTitle);
+
+        return "%s/questions/%d/%s".formatted(reactAppUrl, questionId, questionTitleNormalizer.normalize(questionTitle));
+    }
+}
