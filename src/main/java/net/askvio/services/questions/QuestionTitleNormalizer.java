c60dd460e9456eb704d9e56c9f1aa75c46f69f2e
Houssem Nasri <housi.housi2015@gmail.com>
2022-10-11 19:04:47 +0100

    Create a skeleton for QuestionTitleNormalizer

---
+++ src/main/java/net/askvio/services/questions/QuestionTitleNormalizer.java
@@ -0,0 +1,12 @@
+package net.askvio.services.questions;
+
+import net.askvio.exceptions.NotImplementedException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionTitleNormalizer {
+
+    public String normalize(String title) {
+        throw new NotImplementedException();
+    }
+}
