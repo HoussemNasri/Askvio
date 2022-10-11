45a4a60ed2fdb0cb18bf5a65c5bebb671282f4be
Houssem Nasri <housi.housi2015@gmail.com>
2022-10-11 19:03:14 +0100

    Create UserAccountService
    - I created it primarily because it is the best place to put the getPrincipalUserAccount() method in.

---
+++ src/main/java/net/askvio/services/account/UserAccountService.java
@@ -0,0 +1,27 @@
+package net.askvio.services.account;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import lombok.AllArgsConstructor;
+import net.askvio.database.UserAccountRepository;
+import net.askvio.model.UserAccount;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserAccountService {
+
+    private final UserAccountRepository userAccountRepository;
+
+    public Optional<UserAccount> getPrincipalUserAccount(Authentication authentication) {
+        Objects.requireNonNull(authentication);
+
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        return Optional.ofNullable(principal)
+                       .map(UserDetails::getUsername)
+                       .flatMap(userAccountRepository::findByEmail);
+    }
+}
