6e4deff4fd85a616f5fd2cf42c33556d8faafde2
Houssem Nasri <housi.housi2015@gmail.com>
2022-10-11 19:55:49 +0100

    Implement question title normalization logic

--- src/main/java/net/askvio/services/questions/QuestionTitleNormalizer.java
+++ src/main/java/net/askvio/services/questions/QuestionTitleNormalizer.java
@@ -1,12 +1,25 @@
 package net.askvio.services.questions;
 
+import java.util.Locale;
+import java.util.Objects;
+
 import net.askvio.exceptions.NotImplementedException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionTitleNormalizer {
 
+    // TODO: This needs a lot of testing
     public String normalize(String title) {
-        throw new NotImplementedException();
+        Objects.requireNonNull(title);
+        if (title.isBlank()) {
+            return "";
+        } else {
+            return title.replaceAll("[,/_&.\\\\s]", "-")
+                    .replaceAll("[^-\\w]", "")
+                    // Remove suffix '-'
+                    .replaceAll("-$", "")
+                    .toLowerCase(Locale.ROOT);
+        }
     }
 }
