168c77a13122cebc6599174e12a68ec565281807
Houssem Nasri <housi.housi2015@gmail.com>
2023-01-09 14:59:25 +0100

    Create a fallback error page for unmatched routes

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -8,6 +8,7 @@ import ExploreCommunities from "./pages/ExploreCommunities";
 import {useAppDispatch} from "./redux/app/hooks";
 import {PrivateRoute} from "./router/PrivateRoute";
 import Layout from "./components/Layout";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
     const dispatch = useAppDispatch()
@@ -20,6 +21,7 @@ function App() {
                             <Route path='/' element={<Home/>}/>
                             <Route path='/login' element={<Login/>}/>
                             <Route path='/explore' element={<PrivateRoute><ExploreCommunities/></PrivateRoute>}></Route>
+                            <Route path='*' element={<PageNotFound/>} />
                         </Routes>
                     </Layout>
                 </Router>
---
+++ webapp/src/pages/PageNotFound.tsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <span className="text-[12rem] font-medium">404</span>
+            <span className="text-2xl">Oops! Page Not Found!</span>
+        </div>
+    )
+}
