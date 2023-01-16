3a3929e502e2ae2a793762684122df07a47c6770
Houssem Nasri <housi.housi2015@gmail.com>
2023-01-16 14:46:44 +0100

    Define API response types

---
+++ webapp/src/redux/types.ts
@@ -0,0 +1,36 @@
+export interface FeedResponse {
+    total: number;
+    feed:  QuestionResponse[];
+}
+
+export interface QuestionResponse {
+    id:           number;
+    title:        string;
+    content:      string;
+    creationDate: Date;
+    owner:        UserResponse;
+    community:    CommunityResponse;
+    link:         string;
+}
+
+export interface CommunityResponse {
+    name:         string;
+    id:           number;
+    displayName:  string;
+    primaryTopic: string;
+    about:        string;
+    subtopics:    string[];
+    isPublic:     boolean;
+    isPrivate:    boolean;
+    isRestricted: boolean;
+}
+
+export interface UserResponse {
+    accountId:           number;
+    firstname:           string;
+    lastname:            string;
+    username:            string;
+    email:               string;
+    hasActiveAccount:    boolean;
+    accountCreationDate: Date;
+}
