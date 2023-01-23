827850095baac4aebfe3f06d1fda3c866d9afeee
Houssem Nasri <housi.housi2015@gmail.com>
2023-01-24 00:36:13 +0100

    Show question post owner information

---
+++ webapp/src/components/UserInfo.tsx
@@ -0,0 +1,25 @@
+import {UserResponse} from "../redux/types";
+import {convertToRelativeDate} from "../utils/DateUtils";
+
+interface UserInfoProps {
+    userDetails: UserResponse,
+    action: string,
+    actionTime: Date,
+    isOwner: boolean
+}
+
+export default function UserInfo({userDetails, action, actionTime, isOwner = false}: UserInfoProps) {
+    const {firstname, lastname, avatar} = userDetails
+    return (
+        <div className={`flex flex-col w-52 p-2 gap-1 rounded ${isOwner && 'bg-[#D9EAF7]'}`}>
+            <div className="flex flex-row text-xs text-gray-600 gap-1">
+                <p>{action}</p>
+                <p>{convertToRelativeDate(actionTime)}</p>
+            </div>
+            <div className="flex flex-row align-top gap-2">
+                <img className="w-8 h-8" src={avatar} alt="User avatar"/>
+                <p className="text-sky-700 text-sm">{firstname} {lastname}</p>
+            </div>
+        </div>
+    )
+}
--- webapp/src/pages/QuestionPost.tsx
+++ webapp/src/pages/QuestionPost.tsx
@@ -6,6 +6,7 @@ import {Loader} from "../components/Loader";
 import {convertToRelativeDate} from "../utils/DateUtils";
 import UpvoteDownvote from "../components/UpvoteDownvote";
 import Bookmark from "../components/Bookmark";
+import UserInfo from "../components/UserInfo";
 
 function CommunityHeader() {
     return <div className="flex flex-row p-2 bg-gray-300 fixed top-[72px] left-60 right-0 items-center">
@@ -24,7 +25,7 @@ export default function QuestionPost() {
         console.log(data)
     }, [data])
 
-    return <div className="flex flex-auto flex-col">
+    return <div className="flex flex-auto flex-col max-w-4xl">
         {isLoading ? <Loader/> : (data && <div className="flex flex-row">
             <div className="flex flex-col mr-4 items-center gap-4">
                 <UpvoteDownvote/>
@@ -32,10 +33,10 @@ export default function QuestionPost() {
             </div>
             <div className="flex flex-col">
                 <div className="flex flex-row items-center gap-1">
-                    <img className="w-5 h-5 rounded-full"
+                    {/*<img className="w-5 h-5 rounded-full"
                          src={`https://api.dicebear.com/5.x/bottts/svg?seed=${data.community.name}`}
-                         alt="Rounded avatar"/>
-                    <p className="text-xs font-bold mt-1">
+                         alt="Rounded avatar"/>*/}
+                    <p className="text-xs font-bold text-sky-600 mt-1">
                         v/{data.community.name}
                     </p>
                     {/*<p>·</p>
@@ -43,23 +44,32 @@ export default function QuestionPost() {
                         Posted by u/{data.owner.username} {convertToRelativeDate(data.creationDate)}
                     </p>*/}
                 </div>
-                <h5 className="mb-2 text-2xl font-semibold text-gray-800 tracking-tight text-gray-900 dark:text-white">
+                <h5 className="mb-2 text-3xl font-semibold text-gray-800 tracking-tight text-gray-900 dark:text-white">
                     {data.title}
                 </h5>
-                <div className="flex flex-row gap-4">
-                    <div className="flex flex-row gap-1.5">
-                        <p className="text-gray-500 text-sm">Modified</p>
-                        <p className="text-sm">2 days ago</p>
+                <div className="flex flex-row gap-4 mb-2">
+                    <div className="flex flex-row gap-1.5 text-sm">
+                        <p className="text-gray-600">Modified</p>
+                        <p className="">2 days ago</p>
                     </div>
-                    <div className="flex flex-row gap-1.5">
-                        <p className="text-gray-500 text-sm">Viewed</p>
-                        <p className="text-sm">120 times</p>
+                    <div className="flex flex-row gap-1.5 text-sm">
+                        <p className="text-gray-600">Viewed</p>
+                        <p className="">120 times</p>
                     </div>
                 </div>
                 <hr className="mt-1 mb-3"></hr>
                 <p className="font-normal text-gray-700 dark:text-gray-400 max-w-3xl">
                     {data.content}
                 </p>
+                <div className="flex flex-row mt-16">
+                    <div className="flex flex-row text-gray-600 gap-2 grow text-sm">
+                        <p>Share</p>
+                        <p>Edit</p>
+                        <p>Follow</p>
+                        <p>Flag</p>
+                    </div>
+                    <UserInfo userDetails={data.owner} action="asked" actionTime={data.creationDate} isOwner={true}/>
+                </div>
                 <p className="text-lg mt-10">
                     3 Answers
                 </p>
