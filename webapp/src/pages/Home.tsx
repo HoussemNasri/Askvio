9133b8f1c23288d5f72891cb2c7122aa89561f56
Houssem Nasri <housi.housi2015@gmail.com>
2023-01-27 01:32:08 +0100

    Minor cleanup

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,35 +1,7 @@
 import QuestionCard, {Question} from "../components/QuestionCard";
 import {useGetFeedQuery} from "../redux/feedSlice";
-import {useEffect} from "react";
 
 export default function Home() {
-    const questions: Question[] = [
-        {
-            communityName: "java",
-            title: "",
-            content: "",
-            posterUsername: "",
-            postedAgo: "2 hours ago",
-            commentsCount: 4
-        },
-        {
-            communityName: "java",
-            title: "",
-            content: "",
-            posterUsername: "",
-            postedAgo: "2 hours ago",
-            commentsCount: 4
-        },
-        {
-            communityName: "java",
-            title: "",
-            content: "",
-            posterUsername: "",
-            postedAgo: "2 hours ago",
-            commentsCount: 4
-        },
-    ]
-
     const {data, error, isLoading} = useGetFeedQuery()
 
     return (
