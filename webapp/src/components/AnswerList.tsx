5dd6e26a8d43dc5312103a97bc7af22c8544a754
Houssem Nasri <housi.housi2015@gmail.com>
2023-01-27 01:41:46 +0100

    Create the skeleton of AnswerList component

---
+++ webapp/src/components/AnswerList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {AnswerResponseList} from "../redux/types";
+import {randomInt} from "../utils/RandomUtils";
+
+interface AnswersHeaderProps {
+    answersCount: number
+}
+
+function AnswersHeader({answersCount}: AnswersHeaderProps) {
+    return <div id="answers-header">
+        <p className="text-xl">
+            {answersCount} Answers
+        </p>
+    </div>
+}
+
+interface AnswersProps {
+    questionId: number
+}
+
+export default function AnswerList({questionId}: AnswersProps) {
+    return <div id="answers">
+        <AnswersHeader answersCount={randomInt(10)}/>
+
+    </div>
+}
