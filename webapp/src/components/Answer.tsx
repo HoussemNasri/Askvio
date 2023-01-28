d978ebd964ba812aed5b953004afcefbfbaad9a6
Houssem Nasri <housi.housi2015@gmail.com>
2023-01-28 22:26:35 +0100

    Add Answer.tsx

---
+++ webapp/src/components/Answer.tsx
@@ -0,0 +1,7 @@
+import {AnswerResponse} from "../redux/types";
+import Post from "./Post";
+import React from "react";
+
+export default function Answer(answer: AnswerResponse) {
+    return <Post {...answer}/>
+}
