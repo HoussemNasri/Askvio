6828cba26d0bc679b5132c707458a408edd5b712
Houssem Nasri <housi.housi2015@gmail.com>
2024-05-03 14:44:57 +0100

    Fix formatting

--- src/main/java/net/askvio/database/VoteRepository.java
+++ src/main/java/net/askvio/database/VoteRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface VoteRepository extends JpaRepository<Vote, VoteId> {
-    @Query(value = "SELECT IFNULL(SUM(vote_type), 0) FROM vote WHERE post_id= :postId", nativeQuery = true)
+    @Query(value = "SELECT IFNULL(SUM(vote_type), 0) FROM vote WHERE post_id = :postId", nativeQuery = true)
     Integer getPostTotalVote(@Param("postId") Long postId);
 
     @Query(value = "SELECT COUNT(*) FROM vote WHERE vote_type = 1 AND post_id = :postId", nativeQuery = true)
